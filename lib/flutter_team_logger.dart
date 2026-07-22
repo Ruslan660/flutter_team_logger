1200104c9b2ad6b21c5dc735f4327a264ace8c28
Ruslan <yestemessov.work@gmail.com>
2026-07-22 18:21:57 +0500

    feat(file-storage): JSONL record encoder with size cap

--- lib/flutter_team_logger.dart
+++ lib/flutter_team_logger.dart
@@ -1,4 +1,5 @@
 export 'src/file_storage/file_log_storage_options.dart';
+export 'src/file_storage/log_record_encoder.dart';
 export 'src/file_storage/session_file_name.dart';
 export 'src/log_item.dart';
 export 'src/logs.dart';
