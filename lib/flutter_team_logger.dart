b312f6232ca493002c82551fa2c853bdd1c7fa3d
Ruslan <yestemessov.work@gmail.com>
2026-07-22 18:22:55 +0500

    feat(file-storage): startup retention for session files

--- lib/flutter_team_logger.dart
+++ lib/flutter_team_logger.dart
@@ -1,5 +1,6 @@
 export 'src/file_storage/file_log_storage_options.dart';
 export 'src/file_storage/log_record_encoder.dart';
+export 'src/file_storage/retention.dart';
 export 'src/file_storage/session_file_name.dart';
 export 'src/log_item.dart';
 export 'src/logs.dart';
