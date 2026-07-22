dc97765049b63d2d7adc5ad25d5f87fe7ffbb8ef
Ruslan <yestemessov.work@gmail.com>
2026-07-22 18:24:46 +0500

    feat(file-storage): FileLogStorage publisher with chunk rotation

--- lib/flutter_team_logger.dart
+++ lib/flutter_team_logger.dart
@@ -1,3 +1,4 @@
+export 'src/file_storage/file_log_storage.dart';
 export 'src/file_storage/file_log_storage_options.dart';
 export 'src/file_storage/log_record_encoder.dart';
 export 'src/file_storage/retention.dart';
